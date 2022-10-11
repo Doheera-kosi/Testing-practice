9467408b8c79447408e2ac1229c940a264ad7044
Doheera-kosi <kupourevans99@gmail.com>
2022-10-11 17:14:51 +0000

    Update stringLength.js

--- stringLength.js
+++ stringLength.js
@@ -1,9 +1,10 @@
 // Function to Take arguments and Return the length of the string
 
 function stringLength(string) {
+  if (string.length === 1 && string.length === 10) {
+    return 'Error';
+  }
+
   return [...string].length;
 }
 module.exports = stringLength;
-
-// stringLength('Hello to the whole world');
-// console.log(stringLength('Hello to the whole world'));
